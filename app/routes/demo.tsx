c8b0227894a88e13279becc57c185175237a50d1
mohiuddinmh <mohiuddinmh@gmail.com>
2022-11-23 19:57:26 -0600

    init commit

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function Demo(){
   return<>
-    <h2>Mikki page</h2>
+    <h2>Demo page</h2>
     <Link to="/">Home Page</Link>
   </>
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return <>
-    <h1>Pappa Page</h1>
+    <h1>Home Page</h1>
     <Link to={'/demo'}>Go to Demo</Link>
   </>
 }
