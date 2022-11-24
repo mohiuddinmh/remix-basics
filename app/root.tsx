ebdbf66b3a564de84159ecf12a9714071e7e5f99
mohiuddinmh <mohiuddinmh@gmail.com>
2022-11-23 20:21:46 -0600

    create App comp and Document component

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,32 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Outlet, LiveReload } from '@remix-run/react'
+import React from "react";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+interface DocumentProps {
+  children: React.ReactNode
+  title: string
+}
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+    <Document title='My Remix Blog'>
+      <Outlet />
+    </Document>
+  )
+}
+
+function Document({ children, title }: DocumentProps) {
+  return (
+    <html lang='en'>
+    <head>
+      <title>{title}</title>
+    </head>
+    <body>
+    <h1>Hello There</h1>
+    {children}
+    {process.env.NODE_ENV === 'development'
+      ? <LiveReload />
+      : null}
+    </body>
     </html>
-  );
+  )
 }
